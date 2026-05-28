f9ead5bc43cd1e9d017b3f058c647b1961f914fe
shivampjtii <shivampjtii@gmail.com>
2026-05-28 12:53:51 +0530

    corrected

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,3 @@
   import "./styles/index.css";
 
   createRoot(document.getElementById("root")).render(<App />);
-  
-
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
